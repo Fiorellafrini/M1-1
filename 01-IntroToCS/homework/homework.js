5e271a29360fb646f0c6c510c84820f1b40259df
Constanza <cotyespeche@hotmail.com>
2022-11-07 16:46:19 -0300

    homework 1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,20 +2,39 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-  sum = 0
+  // sum = 0
+  // var numReverse = num.split('').reverse().join('')
+
+
+  // for (i = 0; i < numReverse.length; i++) {
+  //   sum = sum + numReverse[i] * 2 ** numReverse.length - 1;
+  // }
+  // return sum;
+
+  let sum = 0;
   var numReverse = num.split('').reverse().join('')
 
-
-  for (i = 0; i < numReverse.length; i++) {
-    sum = sum + numReverse[i] * 2 ** numReverse.length - 1;
+  for (let i = 0; i < numReverse.length; i++) {
+     sum = sum +numReverse[i] * 2 ** i;
   }
   return sum;
+  
 }
 
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let binary = []
 
+  while (num > 0) {
+    let resto = num % 2
+    binary.unshift(resto)
+    num = Math.floor(num/2)
+    
+  } 
+  binary= binary.join ('');
+  return binary
+  
 }
 
 
